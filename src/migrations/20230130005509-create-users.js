0a53581714d13d770cf2a466733ad0feaf321095
Lerpardo <ls6182315@gmail.com>
2023-01-30 20:29:15 -0300

    users migratios

---
+++ src/migrations/20230130005509-create-users.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const UsersTable = queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      display_name: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      image: {
+        type: Sequelize.STRING,
+      }
+    })
+
+    return UsersTable
+  },
+
+  down: async (queryInterface, Sequelize) => queryInterface.dropTable("users"),
+
+
+};
