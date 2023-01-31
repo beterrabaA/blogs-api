58c377fd5d981e1bf8af9eb9891d9e0508d40111
Lerpardo <ls6182315@gmail.com>
2023-01-31 20:21:27 -0300

    getting user by email

--- src/services/User.js
+++ src/services/User.js
@@ -12,6 +12,25 @@ const getUserLogin = async (email, password) => {
     return { type: '', message: users };
 };
 
+const getUserByEmail = async (email) => {
+    const users = await User.findOne({ where: { email } });
+
+    if (!users) return { type: 'INVALID_LOGIN', message: 'Invalid fields' };
+
+    return { type: '', message: users };
+};
+
+const createUser = async (displayName, email, password, image) => {
+    const error = schema.validateUser({ displayName, email, password, image });
+    if (error.type) return error;
+
+    const newUser = await User.create({ displayName, email, password, image });
+
+    return { type: '', message: newUser };
+};
+
 module.exports = {
     getUserLogin,
+    createUser,
+    getUserByEmail,
 };
