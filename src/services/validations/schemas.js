e78a6461760ca0f05925af77fbe6b9ca730fa296
Lerpardo <ls6182315@gmail.com>
2023-01-31 10:56:35 -0300

    fix

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -1,13 +1,12 @@
 const Joi = require('joi');
 
 const addUserLogin = Joi.object({
-    email: Joi.string().email().empty('').required()
-.messages({
-        'string.email': 'Some required fields are missing',
-        'string.empty': 'Some required fields are missing',
-        'any.required': 'Some required fields are missing',
-    }),
+    email: Joi.string().email().empty('').required(),
     password: Joi.string().empty('').required(),
+}).messages({
+    'string.email': 'Some required fields are missing',
+    'string.empty': 'Some required fields are missing',
+    'any.required': 'Some required fields are missing',
 });
 
 module.exports = {
