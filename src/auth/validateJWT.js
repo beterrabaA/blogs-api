7ec461596d5a52986526d0e86ffe23befc1480fc
Lerpardo <ls6182315@gmail.com>
2023-01-31 20:19:56 -0300

    auth validate jwt

---
+++ src/auth/validateJWT.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../services/User');
+
+const secret = process.env.JWT_SECRET;
+
+module.exports = async (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ message: 'Token not found' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, secret);
+
+    const email = await User.getUserByEmail(decoded.email);
+
+    if (!email) {
+      return res.status(401).json({ message: 'Erro ao procurar usuário do token.' });
+    }
+
+    req.email = email;
+
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Expired or invalid token' });
+  }
+};
