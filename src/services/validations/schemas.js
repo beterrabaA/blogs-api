fbbf5dbdcb4764df1c8098e68dfc46a71ab673fd
Lerpardo <ls6182315@gmail.com>
2023-01-31 10:52:17 -0300

    fixing error validations

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -1,12 +1,13 @@
 const Joi = require('joi');
 
 const addUserLogin = Joi.object({
-    email: Joi.string().email().empty('').required(),
+    email: Joi.string().email().empty('').required()
+.messages({
+        'string.email': 'Some required fields are missing',
+        'string.empty': 'Some required fields are missing',
+        'any.required': 'Some required fields are missing',
+    }),
     password: Joi.string().empty('').required(),
-}).messages({
-    'string.email': 'Some required fields are missing',
-    'string.empty': 'Some required fields are missing',
-    'string.required': 'Some required fields are missing',
 });
 
 module.exports = {
--- src/services/validations/validationsInputValues.js
+++ src/services/validations/validationsInputValues.js
@@ -2,6 +2,7 @@ const { addUserLogin } = require('./schemas');
 
 const validateLogin = (login) => {
     const { error } = addUserLogin.validate(login);
+    console.log(error);
     if (error) return { type: 'INVALID_LOGIN', message: error.message };
 
     return { type: null, message: '' }; 
