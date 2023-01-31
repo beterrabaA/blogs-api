67922c03e1b61edf203eb59d9628766bb32d4051
Lerpardo <ls6182315@gmail.com>
2023-01-31 20:19:29 -0300

    set algorithm token

--- src/utils/token.js
+++ src/utils/token.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 
 const tokenGenerator = (payload) => jwt.sign(payload, JWT_SECRET, {
+    algorithm: 'HS256',
     expiresIn: '1h',
 });
 
