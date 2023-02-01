23480f60177a7054fe3b82e214433206af389605
Lerpardo <ls6182315@gmail.com>
2023-01-31 23:25:07 -0300

    get user by id controller

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -35,8 +35,18 @@ const getAll = async (req, res) => {
     res.status(200).json(message);
 };
 
+const getById = async (req, res) => {
+    const { id } = req.params;
+    const { type, message } = await User.getUserById(id);
+
+    if (type) return res.status(404).json({ message });
+
+    res.status(200).json(message);
+};
+
 module.exports = {
     login,
     createUser,
     getAll,
+    getById,
 };
