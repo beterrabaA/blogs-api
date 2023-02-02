af7fffbdb58acd70da839b777be1a826c2d9c1b7
Lerpardo <ls6182315@gmail.com>
2023-02-02 19:23:11 -0300

    create category controller

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -8,6 +8,20 @@ const getAll = async (_req, res) => {
     res.status(200).json(message);
 };
 
+const createCat = async (req, res) => {
+    const { name } = req.body;
+try {
+    const { type, message } = await Category.createCategory(name);
+
+    if (type) return res.status(400).json({ message });
+
+    res.status(201).json(message);
+} catch (e) {
+    res.status(409).json({ message: 'Category already registered' });
+}
+};
+
 module.exports = {
     getAll,
+    createCat,
 };
