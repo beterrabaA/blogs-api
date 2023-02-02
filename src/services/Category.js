2eeeb176bbd4126c1feb0265fd9329f3eb7a9c38
Lerpardo <ls6182315@gmail.com>
2023-02-02 19:02:40 -0300

    categories services

---
+++ src/services/Category.js
@@ -0,0 +1,22 @@
+const { Category } = require('../models');
+// const schema = require('./validations/validationsInputValues');
+
+const getAll = async () => {
+    const categories = await Category.findAll();
+
+    return { type: '', message: categories };
+};
+
+const createCategory = async (name) => {
+    // const error = schema.validateUser({ displayName, email, password, image });
+    // if (error.type) return error;
+
+    const newCategory = await Category.create({ name });
+
+    return { type: '', message: newCategory };
+};
+
+module.exports = {
+    getAll,
+    createCategory,
+};
