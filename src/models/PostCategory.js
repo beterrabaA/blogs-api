d6fd2e6360f51784bbdafd3d1e8de11e2ce3a439
Lerpardo <ls6182315@gmail.com>
2023-02-03 00:15:53 -0300

    last fix

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,9 +1,13 @@
 module.exports = (sequelize, DataTypes) => {
     const PostCategory = sequelize.define(
         'PostCategory',
-        {},
+        {
+            postId: { type: DataTypes.INTEGER, foreingKey: true, allowNull: false },
+            categoryId: { type: DataTypes.INTEGER, foreingKey: true, allowNull: false },
+        },
         {
             timestamps: false,
+            underscored: true,
             tableName: 'posts_categories',
         }
     );
@@ -25,3 +29,30 @@ module.exports = (sequelize, DataTypes) => {
 
     return PostCategory;
 };
+// module.exports = (sequelize, DataTypes) => {
+//     const PostCategory = sequelize.define(
+//         'PostCategory',
+//         {},
+//         {
+//             timestamps: false,
+//             tableName: 'posts_categories',
+//         }
+//     );
+
+//     PostCategory.associate = (models) => {
+//         models.Category.belongsToMany(models.BlogPost, {
+//             as: 'blog_posts',
+//             through: PostCategory,
+//             foreignKey: 'post_id',
+//             otherKey: 'category_id',
+//         });
+//         models.BlogPost.belongsToMany(models.Category, {
+//             as: 'categories',
+//             through: PostCategory,
+//             foreignKey: 'category_id',
+//             otherKey: 'post_id',
+//         });
+//     };
+
+//     return PostCategory;
+// };
