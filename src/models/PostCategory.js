111bc9dddadd272e23482a78fbc9af6a5bb1c9b6
Lerpardo <ls6182315@gmail.com>
2023-02-03 03:25:08 -0300

    fix response postCategory

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -2,8 +2,8 @@ module.exports = (sequelize, DataTypes) => {
     const PostCategory = sequelize.define(
         'PostCategory',
         {
-            postId: { type: DataTypes.INTEGER },
-            categoryId: { type: DataTypes.INTEGER },
+            postId: { type: DataTypes.INTEGER,foreignKey: true },
+            categoryId: { type: DataTypes.INTEGER,foreignKey: true },
         },
         {
             timestamps: false,
@@ -16,14 +16,14 @@ module.exports = (sequelize, DataTypes) => {
         models.Category.belongsToMany(models.BlogPost, {
             as: 'blog_posts',
             through: PostCategory,
-            foreignKey: 'post_id',
-            otherKey: 'category_id',
+            foreignKey: 'categoryId',
+            otherKey: 'postId',
         });
         models.BlogPost.belongsToMany(models.Category, {
             as: 'categories',
             through: PostCategory,
-            foreignKey: 'category_id',
-            otherKey: 'post_id',
+            foreignKey: 'postId',
+            otherKey: 'categoryId',
         });
     };
 
