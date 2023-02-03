9ed6d6b3ef812d705acd37340d500dd14c76f60d
Lerpardo <ls6182315@gmail.com>
2023-02-03 00:38:55 -0300

    testing mods

--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -2,8 +2,8 @@ module.exports = (sequelize, DataTypes) => {
     const PostCategory = sequelize.define(
         'PostCategory',
         {
-            postId: { type: DataTypes.INTEGER, foreingKey: true, allowNull: false },
-            categoryId: { type: DataTypes.INTEGER, foreingKey: true, allowNull: false },
+            postId: { type: DataTypes.INTEGER },
+            categoryId: { type: DataTypes.INTEGER },
         },
         {
             timestamps: false,
