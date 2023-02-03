f2213f30b3b120af2473e48617cda91a9704c7fe
Lerpardo <ls6182315@gmail.com>
2023-02-03 03:24:52 -0300

    insert new Post

---
+++ src/services/Post.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+const { PostCategory } = require('../models');
+const config = require('../config/config');
+const { BlogPost } = require('../models');
+const schema = require('./validations/validationsInputValues');
+
+const env = process.env.NODE_ENV || 'development';
+
+const sequelize = new Sequelize(config[env]);
+
+const getAll = async () => {
+    const postCategories = await PostCategory.findAll();
+
+    return { type: '', message: postCategories };
+};
+
+const insert = async (postCategory, cats) => {
+    const error = schema.validatePost(postCategory, cats);
+    if (error.type) return error;
+    try {
+        const result = await sequelize.transaction(async (t) => {
+          const post = await BlogPost.create(postCategory, { transaction: t });
+          const categories = cats.map((e) => ({ categoryId: e, postId: post.id }));
+          await PostCategory.bulkCreate(categories, { transaction: t });
+          return post;
+        });
+        return { type: '', message: result };
+      } catch (err) {
+        return { type: 'INVALID_CATEGORY', message: 'one or more "categoryIds" not found' };
+      }
+};
+
+module.exports = {
+    getAll,
+    insert,
+};
